88c61a73157513859e0d271ed466cb3153045723
kenedy4 <kennedymusyoki8@gmail.com>
2024-06-24 12:56:14 +0300

    Failed to pass the tests

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+document.addEventListener("DOMContentLoaded", () => {
+  const mainElement = document.querySelector("main#main");
+  if (mainElement) {
+    mainElement.remove("main#main");
+  }
+  const newHeader = document.createElement("h1");
+  newHeader.id = "newHeader";
+  newHeader.victory = "Kenedy";
+  document.body.appendChild(newHeader);
+  window.newHeader = newHeader;
+});
